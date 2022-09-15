2f1a5532ddea6af1e8e34e434f7afe704b6b8aff
geekbutchic <71285487+geekbutchic@users.noreply.github.com>
2022-09-15 11:56:48 -0600

    adds three image containers to body

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -4,15 +4,44 @@ import "./body.css";
 
 const Body = () => {
   const [products, setProducts] = useState(clothing);
+
   return (
     <div className="body">
-      <div><button className="button-one">Casual</button></div>
-      <div><button className="button-two">Formal</button></div>
-     <div><button className="button-three">Sporty</button></div>
-      {/* {products.clothing.map((product, index) => {
-        console.log(product);
-        return <li key={index}>{product.description}</li>;
-      })} */}
+      <div>
+        <div className="block-one">
+          <h2>What To Wear?</h2>
+        </div>
+        <div className="block-two">Casual - Formal - Sporty</div>
+      </div>
+      <div className="button-container">
+        <button className="button-one">Casual</button>
+        <button className="button-two">Sport</button>
+        <button className="button-three">Formal</button>
+      </div>
+      <div className="outfit-container">
+        <div>
+          <h1 className="top">Top</h1>
+        </div>
+        <div className="top-image">
+          <img src="https://slimages.macysassets.com/is/image/MCY/products/0/optimized/19226370_fpx.tif?$browse$&wid=376&fmt=webp" alt="shirt"></img>
+        </div>
+      </div>
+      <div className="outfit-container-two">
+        <div>
+          <h1 className="bottom">Bottoms</h1>
+        </div>
+        <div className="top-image">
+          <img src="https://slimages.macysassets.com/is/image/MCY/products/0/optimized/19226370_fpx.tif?$browse$&wid=376&fmt=webp" alt="shirt"></img>
+        </div>
+      </div>
+      <div className="outfit-container-three">
+        <div>
+          <h1 className="shoes">Shoes</h1>
+        </div>
+        <div className="top-image">
+          <img src="https://slimages.macysassets.com/is/image/MCY/products/0/optimized/19226370_fpx.tif?$browse$&wid=376&fmt=webp" alt="shirt"></img>
+        </div>
+      </div>
     </div>
   );
 };
